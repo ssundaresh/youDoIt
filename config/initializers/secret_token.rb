1561e21b9526c17ce6cdc2dc0e748f79311513bb
ssundaresh <shruti.sundaresh@gmail.com>
2011-10-19 13:16:10 -0700

    Using rspec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+YouDoIt::Application.config.secret_token = 'be5ece69baddc135f56bd5fd58e0123eb6f19e234e6b2a037f6de2e52a3a21a06fa459cf0557e0d0ff620c1b47770ca5ad56930c9d6d4921005906f9a8c1809c'
